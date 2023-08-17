8bac6a39be6a8ed61c28bd9b29e50154794da9ff
Truong Phuc <tranphuctruong0001@gmail.com>
2023-08-17 01:17:07 -0700

    bai tap Su Dung Toan Tu

---
+++ SuDungToanTu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SuDungToanTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float w;
+            float h;
+
+            Console.Write("Enter width: ");
+            w = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            h = float.Parse(Console.ReadLine());
+
+            float area = w * h;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
