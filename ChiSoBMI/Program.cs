c7ff25581e7d85eacd8dddc9888434bdfce7ab2e
Truong Phuc <tranphuctruong0001@gmail.com>
2023-08-21 06:42:13 -0700

    bt chi so BMI

---
+++ ChiSoBMI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            float w;
+            float h;
+
+            Console.WriteLine("Please enter a height");
+            h = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight");
+            w = float.Parse(Console.ReadLine());
+
+            double bmi = w / Math.Pow(h,2);
+            bmi = Math.Round(bmi, 1);
+
+            Console.Write($"BMI: {bmi}");
+
+            if (bmi < 18)   
+                Console.WriteLine(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+        }
+    }
+
+}
+
