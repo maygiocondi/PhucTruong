dc3c5a1ddec2a6bac138c1d73aca998477a20579
Truong Phuc <tranphuctruong0001@gmail.com>
2023-08-29 22:09:59 -0700

    bai tap buoi 6

---
+++ 06.3-XoaPhanTuKhoiMang/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace XoaPhanTu
+{
+    class XoaPhanTu
+    {
+        static void Main(string[] args)
+        {
+            float[] a = { 4, 66, 33, 63, 11 };
+            XoaPhanTuKhoiMang(a, 11);
+        }
+
+        private static void XoaPhanTuKhoiMang(float[] mang, int soCanXoa)
+        {
+            for (int i = 0; i < mang.Length; i++)
+            {
+                Console.Write(mang[i] + " ");
+            }
+            int indexToRemove = -1;
+            for (int i = 0; i < mang.Length; i++)
+            {
+                if (mang[i] == soCanXoa)
+                {
+                    indexToRemove = i;
+                }
+            }
+            Console.WriteLine();
+            if (indexToRemove == -1)
+            {
+                Console.WriteLine("Ko tim thay gia tri " + soCanXoa);
+                return;
+            }
+
+            for (int i = indexToRemove; i < mang.Length; i++)
+            {
+                if (i == mang.Length - 1)
+                {
+                    mang[i] = 0;
+                }
+                else
+                {
+                    mang[i] = mang[i + 1];
+                }
+            }
+
+            for (int i = 0; i < mang.Length; i++)
+            {
+                Console.Write(mang[i] + " ");
+            }
+        }
+    }
+}
---
+++ 06.5-SoLanXuatHienCuaKyTu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+namespace SoLanXuatHienCuaKyTu
+{
+    class SoLanXuatHienCuaKyTu
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Bạn hãy nhập vào một chuỗi : ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Bạn hãy nhập ký tự cần đếm : ");
+			char c = char.Parse(Console.ReadLine());
+			int answer = 0;
+			for (int i = 0; i < s.Length; i++) {
+				if (s[i] == c) {
+					answer++;
+				}
+			}
+			Console.WriteLine($"Chuỗi {s} có {answer} ký tự {c}" );
+        }
+    }
+}
