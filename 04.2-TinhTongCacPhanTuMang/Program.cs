63a34a449fc41a8d20f124a01bd18aa9110b5204
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-06 00:47:16 -0700

    baitapbuoi4

---
+++ 04.2-TinhTongCacPhanTuMang/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TongPhanTu
+{
+    class TongPhanTu
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = {2, 3, 5, 8};
+            int total = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                total += arr[i];
+            }
+
+            Console.WriteLine($"Tong la : {total}");
+        }
+    }
+}
