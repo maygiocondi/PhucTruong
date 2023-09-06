6cf283bfbce3bdbe34dba185b16c8415a17dc9ca
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-05 22:27:27 -0700

    Bai tap Buoi 2

---
+++ 01.4-HienThiLoiChao/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HienThiLoiChao
+{
+    class HienThiLoiChao
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
