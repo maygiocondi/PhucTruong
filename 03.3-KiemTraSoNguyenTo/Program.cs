3cb30fc029829e6760752003ae1f478335152988
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-06 00:36:48 -0700

    BaiTapNgay3

---
+++ 03.3-KiemTraSoNguyenTo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SoNguyenTo
+{
+    public class SoNguyenTo
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            Console.WriteLine("Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = true;
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+            Console.Write(isPrime);
+        }
+    }
+}
