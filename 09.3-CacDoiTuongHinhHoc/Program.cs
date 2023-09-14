e2acaf44fcf1bc984e60e3b952e1e4136b681295
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-14 03:55:47 -0700

    qwe

---
+++ 09.3-CacDoiTuongHinhHoc/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    static void Main(string[] agrs)
+    {
+       Square square = new Square();
+
+                Console.WriteLine(square);
+
+                square = new Square(2.3);
+
+                Console.WriteLine(square);
+
+                square = new Square(5.8, “yellow”, true);
+
+                Console.WriteLine(square);
+    }
+}
