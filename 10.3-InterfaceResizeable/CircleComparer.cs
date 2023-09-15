b72b6991a13ccf745a94a8900ae312d88bd41dfc
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-15 03:56:05 -0700

    qwe

---
+++ 10.3-InterfaceResizeable/CircleComparer.cs
@@ -0,0 +1,9 @@
+public class CircleComparator : IComparer<Circle>
+{
+    public int Compare(Circle c1, Circle c2)
+    {
+        if (c1.GetRadius() > c2.GetRadius()) return 1;
+        else if (c1.GetRadius() < c2.GetRadius()) return -1;
+        else return 0;
+    }
+}
---
+++ 10.3-InterfaceResizeable/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    static void Main(string[] agrs)
+    {
+        Console.WriteLine("Before Resize: ");
+
+        Circle circle = new Circle(50);
+
+        Console.WriteLine("Circle Radius: " + circle.GetRadius());
+
+        Rectangle rectangle = new Rectangle(50, 100);
+
+        Console.WriteLine("Rectangle width: " + rectangle.GetWidth() + " Length: " + rectangle.GetLength());
+
+        Square square = new Square(50);
+
+        Console.WriteLine("Square Side: " + square.GetSide());
+
+        circle.Resize(10);
+        rectangle.Resize(10);
+        square.Resize(10);
+
+        Console.WriteLine("After Resize: ");
+        Console.WriteLine("Circle Radius: " + circle.GetRadius());
+        Console.WriteLine("Rectangle width: " + rectangle.GetWidth() + "Length: " + rectangle.GetLength());
+        Console.WriteLine("Square Side: " + square.GetSide());
+    }
+}
