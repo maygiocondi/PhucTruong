303dc7fb84789bac5d015d12be4878eca2447f01
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-15 04:28:01 -0700

    asd

---
+++ 11.1-IndexOutOfRangeException/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class Debug
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<Char> characters = new List<char>();
+            characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+            int value = characters[6];
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            System.Console.WriteLine(ex);
+        }
+    }
+}
