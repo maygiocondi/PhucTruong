2270aa3c3ab1238c7e64a05cb3d202dab08a3f9c
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-16 03:58:13 -0700

    qwe

---
+++ 12.1-TinhTongCacSoTrongFileText/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input file path");
+        string path = "C:/Users/Truong Phuc/Desktop/New folder (3)/Test.txt";
+        Console.WriteLine("File path: " + path);
+        ReadTextFileExample example = new ReadTextFileExample();
+        example.ReadTextFile(path);
+        
+    }
+}
