66f71c381f0105a1c30af80923f4947c41b4045f
Truong Phuc <tranphuctruong0001@gmail.com>
2023-09-18 03:41:17 -0700

    qwe

---
+++ 12.4-DocFileCSV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+public class ReadToFileCSV
+{
+    static void Main(string[] args)
+    {
+        List<string> lstA = new List<string>();
+        StreamReader reader = new StreamReader("test.csv");
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            string[] value = line.Split('"');
+            
+            foreach (var val in value)
+            {
+                if (val != ",")
+                {
+                    Console.Write(val + "    ");
+                }
+            }
+            Console.WriteLine();
+        }
+        reader.Close();
+    }
+
+}
